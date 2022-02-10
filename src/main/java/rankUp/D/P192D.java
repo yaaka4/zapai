5d5a7ac3fcd39f8fb6449af50bd8171e07c15052
yaaka <hrk.yszwz@gmail.com>
2022-02-10 23:55:12 +0900

    感嘆符

---
+++ src/main/java/rankUp/D/P192D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P192D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line + "!");
+    }
+}
