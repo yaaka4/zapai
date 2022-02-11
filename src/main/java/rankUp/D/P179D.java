27ab707d25f18ab97ba790f3083ad41bb4f7d210
yaaka <hrk.yszwz@gmail.com>
2022-02-11 22:56:28 +0900

    分け前

---
+++ src/main/java/rankUp/D/P179D.java
@@ -0,0 +1,14 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P179D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var a = sc.nextInt();
+        var b = sc.nextInt();
+        System.out.println(b % a);
+    }
+}
