ac2c532f7dc02e1d445c1190438a4e401e88cfbd
yaaka <hrk.yszwz@gmail.com>
2022-02-12 23:05:51 +0900

    時給

---
+++ src/main/java/rankUp/D/P185D.java
@@ -0,0 +1,12 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P185D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() * sc.nextInt());
+    }
+}
