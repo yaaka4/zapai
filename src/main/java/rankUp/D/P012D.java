99ec4779424956ceeaaead12d9c8b48be17e01e1
yaaka <hrk.yszwz@gmail.com>
2022-02-15 23:45:03 +0900

    絶対値

---
+++ src/main/java/rankUp/D/P012D.java
@@ -0,0 +1,12 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P012D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        System.out.println(Math.abs(sc.nextInt()));
+    }
+}
