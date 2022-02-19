3c1d2b028757946d2a80cf932f0571351868c6c0
yaaka <hrk.yszwz@gmail.com>
2022-02-19 23:46:57 +0900

    偶数奇数

---
+++ src/main/java/rankUp/D/P008D.java
@@ -0,0 +1,17 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P008D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        if(sc.nextInt() % 2 == 0) {
+            System.out.println("even");
+        } else {
+            System.out.println("odd");
+        }
+
+    }
+}
