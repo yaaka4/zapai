f03900c5a906d38b58e97f35fe7a70ff91d9c987
yaaka <hrk.yszwz@gmail.com>
2022-02-20 23:48:45 +0900

    買い物袋

---
+++ src/main/java/rankUp/D/P167D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P167D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var price = sc.nextInt();
+        System.out.println(price >= 1000 ? price + 3 : price);
+    }
+}
