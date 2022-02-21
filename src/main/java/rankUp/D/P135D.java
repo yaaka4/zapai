0fcbf410e9871f4c87c1f158a2ecf059367036f2
yaaka <hrk.yszwz@gmail.com>
2022-02-21 23:48:11 +0900

    多角形

---
+++ src/main/java/rankUp/D/P135D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P135D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println((sc.nextInt() / 180) + 2);
+    }
+}
