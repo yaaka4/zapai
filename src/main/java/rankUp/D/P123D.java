cb5b02eac8f7a3c26a546cbd7150876b97e4a080
yaaka <hrk.yszwz@gmail.com>
2022-02-22 22:43:33 +0900

    自動チャージ

---
+++ src/main/java/rankUp/D/P123D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P123D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var money = sc.nextInt();
+        System.out.println(money < 10000 ? money + 10000 : money);
+    }
+}
