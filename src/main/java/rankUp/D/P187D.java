476c13ddff63ac4bdf0fa82420a53f277efb508c
yaaka <hrk.yszwz@gmail.com>
2022-02-23 23:49:22 +0900

    曲名

---
+++ src/main/java/rankUp/D/P187D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.*;
+
+public class P187D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.next() + "/" + sc.next());
+    }
+}
