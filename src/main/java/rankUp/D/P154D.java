c956c428c445b6b29c60e8f75e5366469d3cf8f0
yaaka <hrk.yszwz@gmail.com>
2022-03-01 23:14:12 +0900

    画面構成

---
+++ src/main/java/rankUp/D/P154D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P154D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var a = sc.nextInt();
+        System.out.println(a * a - sc.nextInt());
+    }
+}
