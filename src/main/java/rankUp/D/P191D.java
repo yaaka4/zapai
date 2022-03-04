7bc4e81999b27953ad2d6c004be745f201627aaf
yaaka <hrk.yszwz@gmail.com>
2022-03-04 23:58:03 +0900

    体温チェック

---
+++ src/main/java/rankUp/D/P191D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P191D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        float line = Float.parseFloat(sc.nextLine());
+        System.out.println(line > 37.0 ? "NG" : "OK");
+    }
+}
