9455f5870df2199d7db53d93fdd57b31521df0a4
yaaka <hrk.yszwz@gmail.com>
2022-03-06 23:32:31 +0900

    充電時間

---
+++ src/main/java/rankUp/D/P032D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P032D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(100 - sc.nextInt());
+    }
+}
