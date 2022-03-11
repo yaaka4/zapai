6d2382e4d1e51f435273770cf066eddcaf65a4d0
yaaka <hrk.yszwz@gmail.com>
2022-03-11 23:40:37 +0900

    日付の表記

---
+++ src/main/java/rankUp/D/P168D.java
@@ -0,0 +1,15 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P168D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var year = sc.nextInt();
+        var month = sc.nextInt();
+        var day = sc.nextInt();
+        System.out.println(month + "/" + day + "/" + year);
+    }
+}
