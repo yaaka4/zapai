e2961b876bec0b3dc83a39ccdebb1e1e3c553de1
yaaka <hrk.yszwz@gmail.com>
2022-03-15 23:39:17 +0900

    忘年会予算

---
+++ src/main/java/rankUp/D/P080D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P080D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() * 6000 + sc.nextInt() * 4000);
+    }
+}
