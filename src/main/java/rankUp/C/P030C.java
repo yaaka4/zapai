ff4bd623c7a9ffe3922a0ea0602b6d345bb400f9
yaaka <hrk.yszwz@gmail.com>
2022-03-21 01:23:28 +0900

    白黒変換リファクタ

--- src/main/java/rankUp/C/P030C.java
+++ src/main/java/rankUp/C/P030C.java
@@ -1,39 +1,32 @@
 package rankUp.C;
 
-import java.util.Arrays;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 public class P030C {
-    final static int WHITE_SCALE = 128;
+    private final static int WHITE_SCALE = 128;
 
     public static void main(String[] args) {
-
         final var sc = new Scanner(System.in);
 
         final var height = sc.nextInt();
-        // スキップ
-        //final var weight =
-        sc.nextInt();
-        // スキップ
+        final var width = sc.nextInt();
+        // 改行スキップ
         sc.nextLine();
 
         IntStream.range(0, height)
-                .mapToObj(i -> returnIntArray(sc))
-                .map(array ->
-                        Arrays.stream(array)
-                            .mapToObj(p -> p >= WHITE_SCALE ? "1" : "0")
-                            .toArray(String[]::new)
-                    )
-                .forEach(array -> System.out.println(String.join(" ", array)));
+                .mapToObj(i -> returnStreamInt(sc, width))
+                .map(sti -> sti.map(p -> p >= WHITE_SCALE ? "1" : "0")
+                               .collect(Collectors.joining(" ")))
+                .forEach(System.out::println);
 
         sc.close();
     }
 
-    private static int[] returnIntArray(Scanner sc) {
-        return Stream.of(sc.nextLine().split(" "))
-                .mapToInt(Integer::parseInt)
-                .toArray();
+    private static Stream<Integer> returnStreamInt(Scanner sc, int width) {
+        return IntStream.range(0, width)
+                .mapToObj(i -> sc.nextInt());
     }
 }
