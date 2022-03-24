9e03c62a16fd66186dc0e3768ea44651cfa5f79a
yaaka <hrk.yszwz@gmail.com>
2022-03-24 23:55:34 +0900

    ProJava12

---
+++ src/main/java/projava/WebClient2.java
@@ -0,0 +1,29 @@
+package projava;
+
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocketFactory;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class WebClient2 {
+    public static void main(String[] args) throws IOException {
+        var domain = "www.google.com";
+        SocketFactory factory = SSLSocketFactory.getDefault();
+        try (var soc = factory.createSocket(domain, 443);
+             var pw = new PrintWriter(soc.getOutputStream());
+             var isr = new InputStreamReader(soc.getInputStream());
+             var bur = new BufferedReader(isr))
+        {
+            pw.println("GET /index.html HTTP/1.1");
+            pw.println("HOST: " + domain);
+            pw.println();
+            pw.flush();
+            bur.lines()
+               .limit(18)
+               .forEach(System.out::println);
+        }
+    }
+}
