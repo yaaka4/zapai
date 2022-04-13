8556b144c5a70846043129cccd7f4e8391985f8a
yaaka <hrk.yszwz@gmail.com>
2022-04-13 23:52:37 +0900

    メダリスト表示

---
+++ src/main/java/rankUp/D/P047D.java
@@ -0,0 +1,15 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P047D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Gold " + sc.next());
+        System.out.println("Silver " + sc.next());
+        System.out.println("Bronze " + sc.next());
+    }
+}
