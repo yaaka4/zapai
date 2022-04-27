e822301da42030a8aa49a07f1732e6a6338f12fe
yaaka <hrk.yszwz@gmail.com>
2022-04-27 23:23:11 +0900

    温度差

---
+++ src/main/java/rankUp/D/P088D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P088D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(Math.abs(sc.nextInt() - sc.nextInt()));
+    }
+}
