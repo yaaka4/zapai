db67fb2b20a7db7fea3df99587046c402d3297fb
yaaka <hrk.yszwz@gmail.com>
2022-04-29 00:05:46 +0900

    かまくらの体積

---
+++ src/main/java/rankUp/D/P056D.java
@@ -0,0 +1,14 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P056D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var r1 = sc.nextInt();
+        var r2 = sc.nextInt();
+        System.out.println(r1 * r1 * r1 - r2 * r2 * r2);
+    }
+}
