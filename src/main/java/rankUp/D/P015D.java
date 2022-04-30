4a5e4781f33050688e4722ac498cfc2f6f9226eb
yaaka <hrk.yszwz@gmail.com>
2022-04-30 23:20:28 +0900

    カウントダウン

---
+++ src/main/java/rankUp/D/P015D.java
@@ -0,0 +1,15 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P015D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var num = sc.nextInt();
+        for(; num > 0; num--) {
+            System.out.println(num);
+        }
+    }
+}
