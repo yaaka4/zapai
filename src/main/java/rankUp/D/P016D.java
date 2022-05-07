8c1f5ffd7a0a6a2a299f77bd20df23afeaa0a56e
yaaka <hrk.yszwz@gmail.com>
2022-05-07 23:57:35 +0900

    N文字目まで出力

---
+++ src/main/java/rankUp/D/P016D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P016D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.next().substring(0, sc.nextInt()));
+    }
+}
