cca3daa833fffc7de85c8b1a0ffb41a87f90c564
yaaka <hrk.yszwz@gmail.com>
2022-05-09 23:40:50 +0900

    長さ一致

---
+++ src/main/java/rankUp/D/P105D.java
@@ -0,0 +1,12 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P105D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.next().length() == sc.next().length() ? "Yes" : "No");
+    }
+}
