138097f3d6d6fb1256d50aaf9f76d63f2eac27d0
yaaka <hrk.yszwz@gmail.com>
2022-05-10 23:59:01 +0900

    文字切り詰め

---
+++ src/main/java/rankUp/D/P111D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P111D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var num = sc.nextInt();
+        System.out.println(sc.next().substring(0, num));
+    }
+}
