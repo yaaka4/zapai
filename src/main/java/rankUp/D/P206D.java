e27fd381d114a0c12fba8d695ffb90172b3b96ab
yaaka <hrk.yszwz@gmail.com>
2022-05-15 23:25:26 +0900

    数値入れ替え

---
+++ src/main/java/rankUp/D/P206D.java
@@ -0,0 +1,14 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P206D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var first = sc.nextInt();
+        var second = sc.nextInt();
+        System.out.println(second + " " + first);
+    }
+}
