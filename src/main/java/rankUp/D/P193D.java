6aae3456df4bc3d2855084f3c77cb15853078c11
yaaka <hrk.yszwz@gmail.com>
2022-05-19 23:49:11 +0900

    電池の本数

---
+++ src/main/java/rankUp/D/P193D.java
@@ -0,0 +1,15 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P193D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        final var a = sc.nextInt();
+        final var b = sc.nextInt();
+        System.out.println(b / a);
+        System.out.println(b % a);
+    }
+}
