167cf56b367516878f8513b68cfc9d1354ce53ed
yaaka <hrk.yszwz@gmail.com>
2022-05-20 23:34:12 +0900

    3倍返し

---
+++ src/main/java/rankUp/D/P061D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P061D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var a = sc.nextInt();
+        System.out.println(a == 0 ? 1 : a * 3);
+    }
+}
