5644013abb353f85ab04d2ea5c5e8419576916fb
yaaka <hrk.yszwz@gmail.com>
2022-05-22 23:28:53 +0900

    縦にする

---
+++ src/main/java/rankUp/D/P163D.java
@@ -0,0 +1,14 @@
+package rankUp.D;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class P163D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        Arrays.stream(line.split("")).forEach(System.out::println);
+    }
+}
