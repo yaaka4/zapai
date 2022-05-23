8c0df60de7b8459489eac5aedde87f15f98347ad
yaaka <hrk.yszwz@gmail.com>
2022-05-23 23:28:32 +0900

    単語の入れ替え

---
+++ src/main/java/rankUp/D/P173D.java
@@ -0,0 +1,14 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P173D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var a = sc.next();
+        var b = sc.next();
+        System.out.println(b + " " + a);
+    }
+}
