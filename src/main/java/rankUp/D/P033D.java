87f5f64d618b8b8b99708977ac1befaeb6923065
yaaka <hrk.yszwz@gmail.com>
2022-05-24 23:48:42 +0900

    頭文字

---
+++ src/main/java/rankUp/D/P033D.java
@@ -0,0 +1,14 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P033D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var a = sc.next().substring(0, 1);
+        var b = sc.next().substring(0, 1);
+        System.out.println(a + "." + b);
+    }
+}
