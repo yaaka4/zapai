68859551dd0c6bb0f4bfff80a599170b0492a20d
yaaka <hrk.yszwz@gmail.com>
2022-05-28 00:00:08 +0900

    ワイン

---
+++ src/main/java/rankUp/D/P195D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P195D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var a = sc.nextInt();
+        System.out.println(a * 1.5);
+    }
+}
