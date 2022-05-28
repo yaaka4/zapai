ae7e42dc8d1da7f8a643f84affa39f4d82896471
yaaka <hrk.yszwz@gmail.com>
2022-05-29 00:23:37 +0900

    for文使わない

--- src/main/java/rankUp/D/P103D.java
+++ src/main/java/rankUp/D/P103D.java
@@ -1,18 +1,15 @@
 package rankUp.D;
 
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Scanner;
+import java.util.*;
 
 public class P103D {
     public static void main(String[] args) {
         // 自分の得意な言語で
         // Let's チャレンジ！！
         Scanner sc = new Scanner(System.in);
-        String[] lineArray = sc.nextLine().split("");
-        for(int i = lineArray.length - 1; i >= 0; i--) {
-            System.out.print(lineArray[i]);
-        }
+        List<String> lineList = Arrays.asList(sc.nextLine().split(""));
+        Collections.reverse(lineList);
+        lineList.forEach(System.out::print);
         System.out.println();
     }
 }
