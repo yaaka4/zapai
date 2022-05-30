7c43ad6056f0ec497c16bafb0c2e974fbf2ea506
yaaka <hrk.yszwz@gmail.com>
2022-05-30 23:35:48 +0900

    正三角形

---
+++ src/main/java/rankUp/D/P039D.java
@@ -0,0 +1,15 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P039D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        var a = sc.nextInt();
+        var b = sc.nextInt();
+        var c = sc.nextInt();
+        System.out.println(a == b ? (b == c ? "YES" : "NO") : "NO");
+    }
+}
