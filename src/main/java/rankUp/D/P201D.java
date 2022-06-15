b4b6b26121731851b783b458f329757be14b9208
yaaka <hrk.yszwz@gmail.com>
2022-06-15 23:30:18 +0900

    節分の用意

---
+++ src/main/java/rankUp/D/P201D.java
@@ -0,0 +1,17 @@
+package rankUp.D;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class P201D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        System.out.println(
+            IntStream.range(0, sc.nextInt())
+                .map(i -> sc.nextInt())
+                .sum()
+        );
+    }
+}
