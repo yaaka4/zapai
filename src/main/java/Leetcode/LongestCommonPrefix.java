1311600aca109eacd041847c5b00182d8f743928
yaaka <hrk.yszwz@gmail.com>
2022-06-23 00:06:56 +0900

    fix:LeetCode_14.

--- src/main/java/Leetcode/LongestCommonPrefix.java
+++ src/main/java/Leetcode/LongestCommonPrefix.java
@@ -29,13 +29,17 @@ public class LongestCommonPrefix {
         for (int i = 2; i < strs.length; i++) {
             var stri = strs[i];
             while(stri.length() > 0) {
-                if(result.length() >= stri.length()) {
+                if(result.length() > stri.length()) {
                     result = result.substring(0, stri.length());
                 }
                 if(!result.equals(stri)) {
                     stri = stri.substring(0, stri.length() - 1);
                     continue;
                 }
+                break;
+            }
+            if(stri.length() == 0) {
+                result = "";
                 break;
             }
         }
@@ -46,6 +50,6 @@ public class LongestCommonPrefix {
     public static void main(String[] args) {
         var array = new String[]{"flower","flow","flight"};
         LongestCommonPrefix lcp = new LongestCommonPrefix();
-        lcp.longestCommonPrefix(array);
+        System.out.println(lcp.longestCommonPrefix(array));
     }
 }
