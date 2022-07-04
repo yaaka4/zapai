d1fe8c1ae9ebc698ec21492f9287307b092c21cb
yaaka <hrk.yszwz@gmail.com>
2022-07-04 23:22:55 +0900

    初詣

---
+++ src/main/java/rankUp/D/P058D.java
@@ -0,0 +1,13 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P058D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("A".repeat(sc.nextInt()) + "B".repeat(sc.nextInt()) + "A".repeat(sc.nextInt()));
+    }
+}
