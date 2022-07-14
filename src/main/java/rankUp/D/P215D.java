f77e6915e5b2cbe23862d46c1b1481965ddc111a
yaaka <hrk.yszwz@gmail.com>
2022-07-14 23:25:15 +0900

    金庫のパスワード

---
+++ src/main/java/rankUp/D/P215D.java
@@ -0,0 +1,12 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P215D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        System.out.println("paiza".equals(sc.next()) ? "Yes" : "No");
+    }
+}
