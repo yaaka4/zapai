b962e92f953b56f64c6b55f98082dfa1def6f7d4
yaaka <hrk.yszwz@gmail.com>
2023-01-11 02:07:33 +0900

    どれだけ増えた

---
+++ src/main/java/rankUp/D/P229D.java
@@ -0,0 +1,12 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P229D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        System.out.println(sc.nextInt() - 10);
+    }
+}
