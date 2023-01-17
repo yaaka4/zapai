b7db5bc5a1d76262cb00e6fd90e6b95ed18b392c
yaaka <hrk.yszwz@gmail.com>
2023-01-17 09:47:24 +0900

    花見の準備

---
+++ src/main/java/rankUp/D/P092D.java
@@ -0,0 +1,31 @@
+package rankUp.D;
+
+import java.util.Scanner;
+import java.util.stream.IntStream;
+
+public class P092D {
+    public static void main(String[] args) {
+        final var sc = new Scanner(System.in);
+
+        final var first = sc.nextLine();
+        final var second = sc.nextLine();
+        final var firstArray = first.split(" ");
+        final var secondArray = second.split(" ");
+
+        final String result;
+        if(calcCost(firstArray) > calcCost(secondArray)) {
+            result = second;
+        } else if(calcCost(firstArray) < calcCost(secondArray)) {
+            result = first;
+        } else {
+            result = "DRAW";
+        }
+
+        System.out.println(result);
+        sc.close();
+    }
+
+    private static double calcCost(String[] sheetArray) {
+        return Double.parseDouble(sheetArray[2]) / (Double.parseDouble(sheetArray[0]) * Double.parseDouble(sheetArray[1]));
+    }
+}
