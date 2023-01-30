27088ce81fc37cee03428439a6dccd7ccee74514
yaaka <hrk.yszwz@gmail.com>
2023-01-31 01:37:18 +0900

    イルミネーションの数

---
+++ src/main/java/rankUp/D/P142D.java
@@ -0,0 +1,12 @@
+package rankUp.D;
+
+import java.util.Scanner;
+
+public class P142D {
+    public static void main(String[] args) {
+        // 自分の得意な言語で
+        // Let's チャレンジ！！
+        Scanner sc = new Scanner(System.in);
+        System.out.println((int)Math.ceil((double)sc.nextInt() / (double)sc.nextInt()) * sc.nextInt());
+    }
+}
